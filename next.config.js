756e849d408e411d65de40de3596103882fec7cb
J.sky <bosichong@qq.com>
2023-08-18 17:00:05 +0800

    更新

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
+  output: {
+    // 添加以下配置以禁用 `/#python` 锚点导致 tailwind 样式无法加载
+    trailingSlash: true,
+  },
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/index.js
+++ pages/index.js
@@ -1,118 +1,170 @@
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import {Inter} from 'next/font/google'
+import {useEffect, useState} from "react";
+import links from '../data';
+import Link from "next/link"; // 导入您的data.js文件
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({subsets: ['latin']})
+// console.log(data)
 
 export default function Home() {
-  return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+
+    const [show, setShow] = useState(true)
+    function hanleClick(){
+        setShow(!show)
+    }
+    const handleScreenLessThan640 = () => {
+        if (window.innerWidth < 768) {
+            setShow(!show);
+        }
+    };
+
+    useEffect(() => {
+        const handleResize = () => {
+            // Update the show state based on window width
+            setShow(window.innerWidth >= 768);
+        };
+
+        // Add event listener for window resize
+        window.addEventListener('resize', handleResize);
+
+        // Initial check on component mount
+        handleResize();
+
+        // Clean up the event listener on component unmount
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
+    return (
+        <div>
+            <main className={`overflow-hidden relative w-full h-screen flex z-0`}>
+                <div className={`fixed z-20 top-0 left-0 flex-shrink-0 h-full overflow-x-hidden bg-gray-900 ${show ? '':'hidden'}`} id={`leftnav`}>
+                    <div className={`h-screen w-[260px]`}>
+                        <div className={`text-white relative h-full w-full flex-1 items-start border-white/20`}>
+                            <nav className={`flex h-full w-full flex-col p-2`}>
+                                <div className="mb-1 flex justify-center items-center gap-2" id={`apptop`}>
+                                    <div className="flex justify-center items-center">
+                                        <h1 className={`text-2xl`}>极 简 导 航</h1>
+                                    </div>
+                                </div>
+                                <div className="flex-1 overflow-hidden hover:overflow-auto" id={`appcontent`}>
+                                    <div className={`pr-2`}>
+                                        <div className="flex flex-col justify-between ">
+                                            <ul className="space-y-1">
+                                                {
+                                                    links.map(({id,data})=>(
+                                                        <li key={id}>
+                                                            <details
+                                                                className="group [&_summary::-webkit-details-marker]:hidden">
+                                                                <summary
+                                                                    className="flex cursor-pointer items-center justify-between rounded-lg px-4 py-2 text-gray-500 hover:bg-gray-100 hover:text-gray-700"
+                                                                >
+                                                                    <span className="text-sm font-medium"> {id} </span>
+
+                                                                    <span
+                                                                        className="shrink-0 transition duration-300 group-open:-rotate-180"
+                                                                    >
+                                                                                  <svg
+                                                                                      xmlns="http://www.w3.org/2000/svg"
+                                                                                      className="h-5 w-5"
+                                                                                      viewBox="0 0 20 20"
+                                                                                      fill="currentColor"
+                                                                                  >
+                                                                                    <path
+                                                                                        fillRule="evenodd"
+                                                                                        d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                                                                        clipRule="evenodd"
+                                                                                    />
+                                                                                  </svg>
+                                                                                </span>
+                                                                </summary>
+
+                                                                <ul className="mt-2 space-y-1 px-4">
+                                                                    {
+                                                                        data.map(({id,item})=>(
+                                                                            <li key={id}>
+                                                                                <a
+                                                                                    href={`#${id}`} onClick={handleScreenLessThan640}
+                                                                                    className="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
+                                                                                >
+                                                                                    {id}
+                                                                                </a>
+                                                                            </li>
+                                                                        ))
+                                                                    }
+                                                                </ul>
+                                                            </details>
+                                                        </li>
+                                                    ))
+                                                }
+                                            </ul>
+                                        </div>
+                                    </div>
+
+                                </div>
+                                <div className="mt-1 flex flex-row justify-center items-center gap-2" id={`appbottom`}>
+                                    <Link href={`https://github.com/Jsky2020/`}>Designed by JianLian</Link>
+                                </div>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+                <div className={`z-10 top-0 mt-0 h-full overflow-y-scroll flex-1`} id={`show`} style={{position: 'absolute', width: show ? 'calc(100% - 270px)' : '100%',left: show ? '270px' : '0px' }}>
+                    <div className={`p-2 mt-8 md:p-4`}>
+                            {
+                                links.map(({id,data}) => (
+                                    <div key={id}>
+                                        {
+                                          data.map(({id, data})=>(
+                                              <div key={id} className={`my-4`}>
+                                                  <h3 id={id} className={`mb-2 text-sm`}>{id}</h3>
+                                                  <div className="grid grid-cols-2 gap-4 md:grid-cols-3 md:gap-4 lg:grid-cols-4 lg:gap-4 xl:grid-cols-5 xl:gap-5 2xl:grid-cols-6 2xl:gap-6">
+                                                  {
+                                                      data.map(({id, link})=>(
+
+                                                          <Link key={id} href={link} target={"_blank"}  className="h-10 rounded-md bg-gray-100 text-center leading-10 shadow-sm transition hover:bg-black hover:text-white my-4 hover:animate-bounce">
+                                                                  {id}
+                                                          </Link>
+
+                                                      ))
+                                                  }
+                                                  </div>
+                                              </div>
+                                          ))
+                                        }
+                                    </div>
+                                ))
+                            }
+                        </div>
+
+                    </div>
+                <div className={`fixed z-30 top-2  h-8 flex justify-center items-center`} style={{ left: show ? '270px' : '0px' }}>
+                    <button type="button" onClick={hanleClick}
+                            className="ml-1 flex h-10 w-10 items-center justify-center focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+                            tabIndex="0">
+                        <svg stroke="currentColor" fill="none" strokeWidth="1.5" viewBox="0 0 24 24"
+                             strokeLinecap="round" strokeLinejoin="round"
+                             className={`h-6 w-6 ${show ? '':'hidden'}`}
+                             height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                            <line x1="18" y1="6" x2="6" y2="18"></line>
+                            <line x1="6" y1="6" x2="18" y2="18"></line>
+                        </svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" className={`icon icon-tabler icon-tabler-menu-2 ${show ? 'hidden':''}`}
+                             width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor"
+                             fill="none" strokeLinecap="round" strokeLinejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                            <path d="M4 6l16 0"></path>
+                            <path d="M4 12l16 0"></path>
+                            <path d="M4 18l16 0"></path>
+                        </svg>
+                    </button>
+                </div>
+            </main>
+
+
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    )
 }
+
